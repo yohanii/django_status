4a662827f71303e3360cb8b0f81f6113d64b0be2
yohanii <dygks924@gmail.com>
2022-10-25 21:54:14 +0900

    first commit

---
+++ status/status/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from status.users.models import User
+from status.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
